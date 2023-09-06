2636571d6be8c406462d0672612c7eb106886c26
Jason Hemann <jason.hemann@gmail.com>
2023-09-06 04:15:03 -0700

    Adding up starter code for two assignments

---
+++ _starter_code/lsp0/lsp0.rkt
@@ -0,0 +1,30 @@
+#lang htdp/bsl
+(require 2htdp/image)
+
+;; Your job is to read the Prologue to How to Design Programs and follow along.
+;; Working as a pair, you will implement the code as they do.
+;; You will be able to follow along and have implemented a little game/simulation.
+;; Follow the prologue precisely.
+
+;; Before you submit, I will expect you to have defined all of the following in this file.
+;;
+;; Functions:
+;; y
+;; picture-of-rocket
+;; sign
+;; picture-of-rocket.v2
+;; picture-of-rocket.v3
+;; picture-of-rocket.v4
+;; picture-of-rocket.v5
+;; distance
+;; picture-of-rocket.v6
+;;
+;; Constants:
+;; ROCKET-CENTER-TO-TOP
+;; HEIGHT
+;; CENTER
+;; WIDTH
+;; MTSCN
+;; ROCKET
+;; V
+
